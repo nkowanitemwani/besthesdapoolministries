30a5e27108b262df47b495ea8accbf37ee873e99
temwani <temwaninkowani@gmail.com>
2025-11-07 11:12:14 +0200

    forgot to add navbar

--- src/app/livestream/page.js
+++ src/app/livestream/page.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./livestream.module.css"
+import Navbar from "../components/navbar";
 
 export default function LiveStream(){
     return(
         <div>
+            <Navbar></Navbar>
             <h1>Video Library</h1>
         </div>
     );
