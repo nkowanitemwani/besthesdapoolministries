a9c3a4bbec3fbd5d692ad84d65b1ff079024e3c4
temwani <temwaninkowani@gmail.com>
2026-01-21 11:04:30 +0200

    add new video library placeholder

--- src/app/videolibrary/page.js
+++ src/app/videolibrary/page.js
@@ -2,12 +2,20 @@ import React from "react";
 import styles from "./videolibarary.module.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import Image from "next/image";
 
 export default function VideoLibrary() {
     return(
         <main>
             <Navbar></Navbar>
-            <h1>Video Library</h1>
+            <section>
+                <h1>Bethesda pool Video Library</h1>
+                <Image
+                src="/images/comingsoon.png"
+                alt="video library coming soon"
+                width={400}
+                height={300}></Image>
+            </section>
             <Footer></Footer>
         </main>
     );
